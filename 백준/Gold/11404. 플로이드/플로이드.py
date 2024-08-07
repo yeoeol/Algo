1b2b72bc687c931bdb51951c02f1e09817e1977b
yeoeol <61906141+yeoeol@users.noreply.github.com>
2024-08-07 22:48:59 +0900

    [Gold IV] Title: 플로이드, Time: 2740 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,30 @@
+import sys
+
+INF = sys.maxsize
+
+n = int(input())
+m = int(input())
+
+graph = [[INF] * (n+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+
+def floyd():
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                graph[i][j] = min(graph[i][k]+graph[k][j], graph[i][j])
+
+floyd()
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if graph[i][j] == INF:
+            print(0, end=' ')
+        else:
+            print(graph[i][j], end=' ')
+    print()
