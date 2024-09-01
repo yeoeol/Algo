ae4ec9d365482d957162677344b78347b7fd54c8
yeoeol <61906141+yeoeol@users.noreply.github.com>
2024-09-01 23:32:39 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 392 ms, Memory: 169752 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,10 +1,20 @@
-s = input()
-q = int(input())
+import sys
+input = sys.stdin.readline
 
+S = input().rstrip()
+
+array = [[0] * 26 for _ in range(len(S)+1)]
+
+for i in range(1, len(S)+1):
+    for j in range(26):
+        if ord(S[i-1])-97 == j:
+            array[i][j] = array[i-1][j] + 1
+        else:
+            array[i][j] = array[i-1][j]
+
+
+q = int(input())
 for _ in range(q):
     a, l, r = input().split()
-    _sum = 0
-    for i in range(int(l), int(r)+1):
-        if s[i] == a:
-            _sum += 1
-    print(_sum)
+    a, l, r = ord(a)-97, int(l), int(r)
+    print(array[r+1][a]-array[l][a])
