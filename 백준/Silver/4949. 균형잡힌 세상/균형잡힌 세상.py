d1651455808d1ad776e1ff1eb6bc322dab9c5452
yeoeol <61906141+yeoeol@users.noreply.github.com>
2024-10-31 03:04:03 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -3,30 +3,26 @@ input = sys.stdin.readline
 
 while True:
     stack = []
-    flag = True
     s = input().rstrip()
     if s == '.':
         break
     for i in s:
         if i == '(' or i == '[':
             stack.append(i)
+        if not stack and (i == ']' or i == ')'):
+            stack.append('')
+            break
         elif i == ')':
-            if not stack:
-                flag = False
-                break
-            if stack[-1] == '(':
+            if stack and stack[-1] == '(':
                 stack.pop()
             else:
                 break
         elif i == ']':
-            if not stack:
-                flag = False
-                break
-            if stack[-1] == '[':
+            if stack and stack[-1] == '[':
                 stack.pop()
             else:
                 break
-    if stack or not flag:
+    if stack:
         print("no")
     else:
         print("yes")
