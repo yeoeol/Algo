da835a3f982aec3567820777662ab5978f727e0d
yeoeol <61906141+yeoeol@users.noreply.github.com>
2024-11-01 02:04:06 +0900

    [Silver IV] Title: 덱 2, Time: 1396 ms, Memory: 70604 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.py
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -13,15 +13,15 @@ for i in range(n):
     elif o == 2:
         deque.append(order[1])
     elif o == 3:
-        print(deque.popleft()) if deque else print(-1)
+        print(deque.popleft() if deque else -1)
     elif o == 4:
-        print(deque.pop()) if deque else print(-1)
+        print(deque.pop() if deque else -1)
     elif o == 5:
         print(len(deque))
     elif o == 6:
         print(0) if deque else print(1)
     elif o == 7:
-        print(deque[0]) if deque else print(-1)
+        print(deque[0] if deque else -1)
     elif o == 8:
-        print(deque[-1]) if deque else print(-1)
+        print(deque[-1] if deque else -1)
 
