990ddd84be2b9605fb7f67a4fa3db941ad01b7eb
yeoeol <61906141+yeoeol@users.noreply.github.com>
2024-11-01 01:06:09 +0900

    [Silver IV] Title: 카드2, Time: 212 ms, Memory: 54996 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -4,15 +4,10 @@ from collections import deque
 input = sys.stdin.readline
 
 n = int(input().strip())
-if n == 1:
-    print(n)
-    exit(0)
 queue = deque([i for i in range(1, n+1)])
 
-while len(queue) > 0:
+while len(queue) > 1:
     queue.popleft()
-    if len(queue) == 1:
-        break
     queue.append(queue.popleft())
 
 print(queue[0])
