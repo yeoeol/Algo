7ea978fdf39ae447e8d721b23a04dc4236cfadeb
yeoeol <61906141+yeoeol@users.noreply.github.com>
2024-11-03 02:07:22 +0900

    [Silver V] Title: 방 번호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,20 +1,13 @@
 s = input()
-lst = [0] * 9
+lst = [0] * 10
 
 for i in list(s):
-    if int(i) == 6 or int(i) == 9:
-        lst[6] += 1
+    if i == '6' or i == '9':
+        if lst[6] <= lst[9]:
+            lst[6] += 1
+        else:
+            lst[9] += 1
     else:
         lst[int(i)] += 1
 
-M = max(lst)
-c = lst.count(M)
-if len(s) == 1:
-    print(M)
-elif len(s) != 0 and M == lst[6] and c == 1:
-    if M % 2 == 0:
-        print(M//2)
-    else:
-        print(M//2+1)
-else:
-    print(M)
+print(max(lst))
