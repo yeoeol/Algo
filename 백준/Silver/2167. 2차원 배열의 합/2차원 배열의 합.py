cc58a1096792d4a029b7a57db2a23ab85af9a669
yeoeol <61906141+yeoeol@users.noreply.github.com>
2024-11-05 22:43:28 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 680 ms, Memory: 37032 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,19 +1,18 @@
 n, m = map(int, input().split())
-arr = []
-for i in range(n):
-    arr.append(list(map(int, input().split())))
+array_2d = [] # 2차원 배열 저장
+for _ in range(n):
+    tmp = list(map(int, input().split()))
+    array_2d.append(tmp)
 
-dp = [[0]*(m+1) for _ in range(n+1)]
+dp = [[0]*(m+1) for _ in range(n+1)] # 메모제이션 DP 리스트 생성
+# dp[a][b] = array_2d에서 인덱스 (a-1, b-1)까지 모두 더한 값
+
 for i in range(1, n+1):
-    dp[i][1] = arr[i-1][0]
-    for j in range(2, m+1):
-        dp[i][j] = dp[i][j-1] + arr[i-1][j-1]
-
-
-k = int(input())
-for _ in range(k):
+    for j in range(1, m+1):
+        dp[i][j] = array_2d[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+        # ex dp[2][2] = array_2d[0:1][0:1]의 합
+        
+t = int(input())
+for _ in range(t):
     i, j, x, y = map(int, input().split())
-    _sum = 0
-    for d in range(i, x+1):
-        _sum += dp[d][y]-dp[d][j-1]
-    print(_sum)
+    print(dp[x][y] - dp[i-1][y] - dp[x][j-1] + dp[i-1][j-1]) # i-1, j-1을 빼주어야 i, j부터 남음
