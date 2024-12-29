ee7c6e69f0fc338f501e076682b1100731997844
yeoeol <61906141+yeoeol@users.noreply.github.com>
2024-12-30 01:12:12 +0900

    [Silver I] Title: 트럭, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -0,0 +1,14 @@
+n, w, L = map(int, input().split())
+trucks = list(map(int, input().split()))
+time = 0
+bridge = [0] * w
+
+while bridge:
+    time += 1
+    bridge.pop(0)
+    if trucks:
+        if sum(bridge) + trucks[0] <= L:
+            bridge.append(trucks.pop(0))
+        else:
+            bridge.append(0)
+print(time)
