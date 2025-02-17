40ee5c7ea8edfedfca2219755138ee237401a50a
yeoeol <61906141+yeoeol@users.noreply.github.com>
2025-02-17 17:19:52 +0900

    [Gold IV] Title: 빙산, Time: 4728 ms, Memory: 35052 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,6 +1,7 @@
 import sys
+from collections import deque
+
 input = sys.stdin.readline
-sys.setrecursionlimit(10**4)
 
 d = [(0, 1), (0, -1), (1, 0), (-1, 0)] # 동서남북
 
@@ -8,9 +9,8 @@ d = [(0, 1), (0, -1), (1, 0), (-1, 0)] # 동서남북
 n, m = map(int, input().strip().split())
 iceberg = [list(map(int, input().strip().split())) for _ in range(n)]
 
-year = 0
-while True:
-    melt = [[0]*m for _ in range(n)]
+def melt_num(iceberg, n, m):
+    melt = []
     for i in range(n):
         for j in range(m):
             if iceberg[i][j] != 0:
@@ -20,38 +20,39 @@ while True:
                     if 0 <= nx < n and 0 <= ny < m:
                         if iceberg[nx][ny] == 0:
                             cnt += 1
-                melt[i][j] = cnt
+                melt.append((i, j, cnt))
+    for i, j, cnt in melt:
+        iceberg[i][j] = max(0, iceberg[i][j]-cnt)
 
-    for i in range(n):
-        for j in range(m):
-            if iceberg[i][j] != 0:
-                iceberg[i][j] = max(0, iceberg[i][j]-melt[i][j])
-    year += 1
-
-    def dfs(iceberg, x, y, visited):
-        visited[x][y] = True
+def bfs(iceberg, x, y, visited):
+    queue = deque([(x, y)])
+    visited[x][y] = True
+    while queue:
+        px, py = queue.popleft()
         for i in range(4):
-            nx, ny = x+d[i][0], y+d[i][1]
+            nx, ny = px+d[i][0], py+d[i][1]
             if 0 <= nx < n and 0 <= ny < m:
                 if iceberg[nx][ny] != 0 and not visited[nx][ny]:
-                    dfs(iceberg, nx, ny, visited)
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
 
-    def check(iceberg, n, m):
-        visited = [[False]*m for _ in range(n)]
-        flag = False
-        for i in range(n):
-            for j in range(m):
-                if iceberg[i][j] != 0 and not visited[i][j]:
-                    if flag:
-                        return True
-                    flag = True
-                    dfs(iceberg, i, j, visited)
-        return False
+year = 0
+while True:
+    melt_num(iceberg, n, m)
+    year += 1
+    
+    ice_cnt = 0 # 덩어리 수
+    visited = [[False]*m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if iceberg[i][j] != 0 and not visited[i][j]:
+                ice_cnt += 1
+                bfs(iceberg, i, j, visited)
 
+    if ice_cnt > 1:
+        print(year)
+        break
+    
     if all(iceberg[i][j] == 0 for i in range(n) for j in range(m)):
         print(0)
-        break
-
-    if check(iceberg, n, m):
-        print(year)
         break
