2973a435ed625a4892187244eeb2cc6cf0dd5bfb
yeoeol <61906141+yeoeol@users.noreply.github.com>
2025-02-26 15:44:42 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.07 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,15 +1,14 @@
 def solution(s):
-    answer = ''
     splits = s.split(" ")
+    lst = []
     for sp in splits:
-        lst = []
         for i in range(len(sp)):
             if i % 2 == 0:
                 lst.append(sp[i].upper())
             else:
                 lst.append(sp[i].lower())
-        answer += ''.join(lst)+" "
-        
+        lst.append(' ')
+    answer = ''.join(lst)
     return answer[:-1]
 
 solution("try   hello  world")
