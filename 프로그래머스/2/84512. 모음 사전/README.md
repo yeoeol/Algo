ab01720b9a056d248a4eaac385be64125e957c80
yeoeol <61906141+yeoeol@users.noreply.github.com>
2025-03-22 00:28:35 +0900

    [level 2] Title: 모음 사전, Time: 3.75 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512#) 
 
 ### 성능 요약
 
-메모리: 9.18 MB, 시간: 4.57 ms
+메모리: 9.3 MB, 시간: 3.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 22일 00:22:40
+2025년 03월 22일 00:28:31
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,19 +1,19 @@
 arr = ['A', 'E', 'I', 'O', 'U']
 lst = []
-def choose(idx, n, dic):
+def choose(n, dic):
     if len(lst) == n:
-        dic.append(list(lst))
+        dic.append(''.join(lst))
         return
 
     for w in arr:
         lst.append(w)
-        choose(idx+1, n, dic)
+        choose(n, dic)
         lst.pop()
 
 def solution(word):
     dic = []
 
     for i in range(1, 6):
-        choose(0, i, dic)
-    dic.sort()
-    return dic.index(list(word))+1
+        choose(i, dic)
+    
+    return sorted(dic).index(word)+1
