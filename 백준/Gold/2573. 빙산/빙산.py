111cdb68fecbbc35e32da39c1bb540e5bca78fa4
yeoeol <61906141+yeoeol@users.noreply.github.com>
2025-03-24 00:24:45 +0900

    [Gold IV] Title: 빙산, Time: 3672 ms, Memory: 35068 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,58 +1,81 @@
-import sys
 from collections import deque
 
-input = sys.stdin.readline
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-d = [(0, 1), (0, -1), (1, 0), (-1, 0)] # 동서남북
+ans = 0
 
-# 빙산의 높이는 동서남북에 0이 저장된 칸의 개수만큼 줄어듦
-n, m = map(int, input().strip().split())
-iceberg = [list(map(int, input().strip().split())) for _ in range(n)]
+dxs = [0, 0, 1, -1] # 동서남북
+dys = [1, -1, 0, 0]
+# 각 칸을 순회하면서 0이 아닌 0이 아닌 값의 위치에서
+# 동서남북을 확인하여 0의 개수를 next_grid에 저장
+# grid의 각 값에 next_grid 각 값을 빼기(뺸 값이 0보다 작다면 0으로 고정)
+def get_zero(x, y):
+    cnt = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x+dx, y+dy
+        if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == 0:
+            cnt += 1
+    return cnt
 
-def melt_num(iceberg, n, m):
+def simul(): # O(nm)
     melt = []
     for i in range(n):
         for j in range(m):
-            if iceberg[i][j] != 0:
-                cnt = 0
-                for dx, dy in d:
-                    nx, ny = i+dx, j+dy
-                    if 0 <= nx < n and 0 <= ny < m:
-                        if iceberg[nx][ny] == 0:
-                            cnt += 1
+            if grid[i][j] != 0:
+                cnt = get_zero(i, j)
                 melt.append((i, j, cnt))
-    for i, j, cnt in melt:
-        iceberg[i][j] = max(0, iceberg[i][j]-cnt)
 
-def bfs(iceberg, x, y, visited):
-    queue = deque([(x, y)])
+    for i, j, c in melt:
+        grid[i][j] = max(0, grid[i][j]-c)
+
+# 위 과정(simul)을 한 번 수행할 때마다 bfs를 실행하여 덩어리가 분리되었는지 확인
+def bfs(x, y, visited): # O(nm)
     visited[x][y] = True
+    queue = deque([(x, y)])
     while queue:
-        px, py = queue.popleft()
-        for i in range(4):
-            nx, ny = px+d[i][0], py+d[i][1]
-            if 0 <= nx < n and 0 <= ny < m:
-                if iceberg[nx][ny] != 0 and not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    queue.append((nx, ny))
+        x, y = queue.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x+dx, y+dy
+            if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] != 0 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                queue.append((nx, ny))
 
-year = 0
-while True:
-    melt_num(iceberg, n, m)
-    year += 1
-    
-    ice_cnt = 0 # 덩어리 수
+# 빙산이 분리되었다면 -1
+# 분리되지 않았다면 0
+# 모두 녹았다면 1
+def check(): # O(n^2 * m^2)
+    zero_flag = True
+
     visited = [[False]*m for _ in range(n)]
+    ice_cnt = 0
+
     for i in range(n):
         for j in range(m):
-            if iceberg[i][j] != 0 and not visited[i][j]:
-                ice_cnt += 1
-                bfs(iceberg, i, j, visited)
+            if grid[i][j] != 0:
+                zero_flag = False
+                if not visited[i][j]:
+                    ice_cnt += 1
+                    if ice_cnt >= 2:
+                        return -1
+                    bfs(i, j, visited)
 
-    if ice_cnt > 1:
-        print(year)
+    if zero_flag:
+        return 1
+    return 0
+
+
+while True:
+    simul() # O(nm)
+    ans += 1
+
+    # 빙산이 분리되었다면 -1
+    # 분리되지 않았다면 0
+    # 모두 녹았다면 1
+    val = check() # O(n^2 * m^2)
+    if val == -1:
+        print(ans)
         break
-    
-    if all(iceberg[i][j] == 0 for i in range(n) for j in range(m)):
+    elif val == 1:
         print(0)
         break
