04460adcd40fce315996747d922fea4f8eec41b0
yeoeol <61906141+yeoeol@users.noreply.github.com>
2025-03-24 20:30:07 +0900

    [Silver IV] Title: 수들의 합 2, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+
+ans = 0
+
+sum_val = 0
+j = 0
+for i in range(1, n+1):
+    while j+1 <= n and sum_val < m:
+        sum_val += arr[j+1]
+        j += 1
+
+    if sum_val == m:
+        ans += 1
+
+    sum_val -= arr[i]
+
+print(ans)
