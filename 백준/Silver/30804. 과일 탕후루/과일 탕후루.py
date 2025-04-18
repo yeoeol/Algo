e154efc9546859502920a257048d42e6ed91c498
yeoeol <61906141+yeoeol@users.noreply.github.com>
2025-04-18 12:25:19 +0900

    [Silver II] Title: 과일 탕후루, Time: 440 ms, Memory: 36956 KB -BaekjoonHub

---
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -0,0 +1,32 @@
+import sys
+from collections import defaultdict
+sys.setrecursionlimit(10**6)
+def input():
+    return sys.stdin.readline().strip()
+
+
+n = int(input())
+arr = [0]+list(map(int, input().split()))
+
+cnt = defaultdict(int)
+
+ans = 0
+j = 0
+for i in range(1, n+1):
+    while j+1 <= n and len(cnt) <= 2:
+        cnt[arr[j+1]] += 1
+        if len(cnt) >= 3:
+            del cnt[arr[j+1]]
+            break
+        j += 1
+
+    ans = max(ans, sum(cnt.values()))
+
+    if j+1 > n:
+        break
+
+    cnt[arr[i]] -= 1
+    if cnt[arr[i]] == 0:
+        del cnt[arr[i]]
+
+print(ans)
