1824d0c762377c166992e261026a96a639a6c437
yeoeol <61906141+yeoeol@users.noreply.github.com>
2025-07-27 15:42:23 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,10 +1,11 @@
 n = int(input())
-if n == 1:
-    print(1)
-    exit()
-dp = [0 for _ in range(n+1)]
+
+dp = [0] * 1001
 dp[1] = 1
 dp[2] = 3
+
 for i in range(3, n+1):
     dp[i] = (dp[i-1]+2*dp[i-2])%10007
-print(dp[-1])
+
+print(dp[n])
+
