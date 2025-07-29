0124fbbbd3d8e6c8fdddfd54ca3facb20f88e8f2
yeoeol <61906141+yeoeol@users.noreply.github.com>
2025-07-29 16:35:08 +0900

    [Silver III] Title: 근손실, Time: 76 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.py
+++ 백준/Silver/18429. 근손실/근손실.py
@@ -1,15 +1,22 @@
-import itertools
-
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
-ans = 0
-for a in itertools.permutations(arr, n):
-    kg = 500
-    for item in a:
-        kg += item
-        kg -= k
-        if kg < 500:
-            break
-    else:
+
+visited = [False for _ in range(n)]
+cnt, ans = 0, 0
+def dfs(w):
+    global cnt, ans
+    if w < 500:
+        return
+    if cnt == n:
         ans += 1
+        return
+
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            cnt += 1
+            dfs(w+arr[i]-k)
+            visited[i] = False
+            cnt -= 1
+dfs(500)
 print(ans)
