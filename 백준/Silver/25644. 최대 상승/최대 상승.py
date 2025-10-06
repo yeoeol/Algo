e1e2194c881d2e2f44401202ae52ef254b27ed83
yeoeol <61906141+yeoeol@users.noreply.github.com>
2025-10-06 15:35:25 +0900

    [Silver V] Title: 최대 상승, Time: 164 ms, Memory: 56452 KB -BaekjoonHub

---
+++ 백준/Silver/25644. 최대 상승/최대 상승.py
@@ -0,0 +1,12 @@
+n = int(input())
+prices = list(map(int, input().split()))
+
+min_price = prices[0]
+max_profit = 0
+
+for price in prices:
+    profit = price-min_price
+    max_profit = max(max_profit, profit)
+
+    min_price = min(min_price, price)
+print(max_profit)
