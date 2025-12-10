86745eb307558a38daae7c584a69956b5192aa0a
yeoeol <61906141+yeoeol@users.noreply.github.com>
2025-12-10 14:03:37 +0900

    [level 2] Title: 광물 캐기, Time: 3.23 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/README.md
+++ 프로그래머스/2/172927. 광물 캐기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 광물 캐기 - 172927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927#) 
 
 ### 성능 요약
 
-메모리: 9.06 MB, 시간: 0.08 ms
+메모리: 9.27 MB, 시간: 3.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 10일 14:00:23
+2025년 12월 10일 14:03:35
 
 ### 문제 설명
 
--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,57 +1,43 @@
-from collections import deque
+# 곡괭이 종류: 0=diamond, 1=iron, 2=stone
+tired = [
+    [1, 1, 1],      # diamond pick
+    [5, 1, 1],      # iron pick
+    [25, 5, 1]      # stone pick
+]
 
-dic = {
-    "diamond":0,
-    "iron":1,
-    "stone":2
-}
+dic = {"diamond":0, "iron":1, "stone":2}
 
-tired = {
-    0:[1, 1, 1],
-    1:[5, 1, 1],
-    2:[25, 5, 1]
-}
-
-
-def dig(x, pos, minerals):
-    res = 0
-    for k in range(pos, pos+5):
-        if k >= len(minerals):
-            break
-        cur_mineral = minerals[k]
-        res += tired[x][dic[cur_mineral]]
-    return res
-
-def calc_weight(minerals):
-    arr = []
-    for m in range(0, len(minerals), 5):
-        start = m
-        end = m+5 if m+5 < len(minerals) else len(minerals)
-        weight = 0
-        for i in range(start, end):
-            if minerals[i] == "diamond":
-                weight += 25
-            elif minerals[i] == "iron":
-                weight += 5
-            else:
-                weight += 1
-        arr.append((weight, start))
-    return arr
-    
 def solution(picks, minerals):
-    arr = calc_weight(minerals)
-    print(arr)
-    if sum(picks)*5 < len(minerals):
-        arr.pop()
-    arr.sort()
-    
-    ans = 0
-    i = 0
-    while i <= 2 and arr:
-        if picks[i] == 0:
-            i += 1
-            continue
-        _, start = arr.pop()
-        picks[i] -= 1
-        ans += dig(i, start, minerals)
-    return ans
+    n = len(minerals)
+    answer = float('inf')
+
+    def dfs(idx, picks, fatigue):
+        nonlocal answer
+
+        # 모든 광물을 캐거나, 더 이상 곡괭이가 없으면 종료
+        if idx >= n or sum(picks) == 0:
+            answer = min(answer, fatigue)
+            return
+
+        # 현재까지의 피로도가 이미 최소값보다 크면 가지치기
+        if fatigue >= answer:
+            return
+
+        # 곡괭이 3종류 중 하나를 선택
+        for p in range(3):
+            if picks[p] == 0:
+                continue
+
+            new_picks = picks[:]
+            new_picks[p] -= 1
+
+            # 이번에 p곡괭이로 캐는 피로도 계산
+            add_fatigue = 0
+            for k in range(idx, min(idx+5, n)):
+                m = dic[minerals[k]]
+                add_fatigue += tired[p][m]
+
+            dfs(idx+5, new_picks, fatigue + add_fatigue)
+
+    dfs(0, picks, 0)
+    return answer
