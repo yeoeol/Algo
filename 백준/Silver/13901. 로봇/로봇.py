857e91fe17deff139114edfa8c186fc7e0e0b30c
yeoeol <61906141+yeoeol@users.noreply.github.com>
2025-12-24 02:05:55 +0900

    [Silver I] Title: 로봇, Time: 80 ms, Memory: 35332 KB -BaekjoonHub

--- 백준/Silver/13901. 로봇/로봇.py
+++ 백준/Silver/13901. 로봇/로봇.py
@@ -23,19 +23,9 @@ def in_range(x, y):
     return 0 <= x < r and 0 <= y < c
 
 
-def is_possible(x, y):
-    sets = set(arr)
-    for s in sets:
-        dx, dy = d[s-1]
-        nx, ny = x+dx, y+dy
-        if in_range(nx, ny) and not grid[nx][ny]:
-            return True
-    return False
-
-
 while True:
-    if not is_possible(x, y):
-        break
+    is_moved = False
+    
     for i in range(len(arr)):
         dx, dy = d[arr[i]-1]
         while True:
@@ -44,7 +34,10 @@ while True:
                 break
             if grid[nx][ny]:
                 break
+            is_moved = True
             grid[nx][ny] = True
             x, y = nx, ny
-
+            
+    if not is_moved:
+        break
 print(x, y)
