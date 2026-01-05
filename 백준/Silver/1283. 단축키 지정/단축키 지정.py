0ea9c30c4f6b346eca74a9c4fd66d8b3a5c2fc3c
yeoeol <61906141+yeoeol@users.noreply.github.com>
2026-01-06 02:55:43 +0900

    [Silver I] Title: 단축키 지정, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1283. 단축키 지정/단축키 지정.py
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.py
@@ -6,38 +6,30 @@ def input():
 
 def solution(sets, s):
     split = s.split()
-    idx = 0
-    flag = False
-    # 단어의 첫 글자가 sets에 없다면 sets에 넣고 몇 번째 단어인지 인덱스 저장
+    arr = []
+    # 단어의 첫 글자가 sets에 없다면 단축키 지정
     for i, word in enumerate(split):
         if word[0].upper() not in sets:
-            idx = i
-            flag = True
-            break
-    # 첫 번째 글자 단축키 지정
-    arr = []
-    if flag:
-        for i, word in enumerate(split):
-            if i == idx:
-                sets.add(word[0].upper())
-                arr.append("["+word[0]+"]"+word[1:])
-            else:
-                arr.append(word)
-        return arr
-    else:
-        # 왼쪽에서부터 차례대로 알파벳 순회, 단축키로 지정안된 것이 있다면 지정
-        flag = False
-        for i, word in enumerate(split):
-            temp = ""
-            for j in range(len(word)):
-                if not flag and word[j].upper() not in sets:
-                    sets.add(word[j].upper())
-                    temp += "["+word[j]+"]"
-                    flag = True
+            for j, w in enumerate(split):
+                if i == j:
+                    sets.add(w[0].upper())
+                    arr.append("["+w[0]+"]"+w[1:])
                 else:
-                    temp += word[j]
-            arr.append(temp)
-        return arr
+                    arr.append(w)
+            return arr
+
+    # 왼쪽에서부터 차례대로 알파벳 순회, 단축키로 지정안된 것이 있다면 지정
+    for i, word in enumerate(split):
+        for j in range(len(word)):
+            if word[j].upper() not in sets:
+                for k, w in enumerate(split):
+                    if i == k:
+                        sets.add(w[j].upper())
+                        arr.append(w[:j] + "["+w[j]+"]" + w[j+1:])
+                    else:
+                        arr.append(w)
+                return arr
+    return [s]
 
 n = int(input())
 sets = set()
