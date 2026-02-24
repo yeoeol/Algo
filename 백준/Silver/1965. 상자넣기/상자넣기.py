29280cf143f39c3598a8dbf43501100d10b5280c
yeoeol <61906141+yeoeol@users.noreply.github.com>
2026-02-24 16:02:41 +0900

    [Silver II] Title: 상자넣기, Time: 36 ms, Memory: 34476 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.py
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -1,13 +1,27 @@
+# n = int(input())
+# arr = list(map(int, input().split()))
+#
+# # dp[i] : i에서 끝나는 최장 수열의 길이
+# dp = [0] * n
+#
+# for i in range(n):
+#     dp[i] = 1
+#     for j in range(i):
+#         if arr[i] > arr[j]:
+#             dp[i] = max(dp[i], dp[j]+1)
+#
+# print(max(dp))
+from bisect import bisect_left
+
 n = int(input())
 arr = list(map(int, input().split()))
 
-# dp[i] : i에서 끝나는 최장 수열의 길이
-dp = [0] * n
+lis = []
+for num in arr:
+    pos = bisect_left(lis, num)
+    if pos == len(lis):
+        lis.append(num)
+    else:
+        lis[pos] = num
 
-for i in range(n):
-    dp[i] = 1
-    for j in range(i):
-        if arr[i] > arr[j]:
-            dp[i] = max(dp[i], dp[j]+1)
-
-print(max(dp))
+print(len(lis))
