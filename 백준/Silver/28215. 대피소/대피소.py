210e7edf409a178e32d77ade1e536c08891b212f
yeoeol <61906141+yeoeol@users.noreply.github.com>
2026-03-19 14:57:36 +0900

    [Silver IV] Title: 대피소, Time: 1624 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.py
+++ 백준/Silver/28215. 대피소/대피소.py
@@ -3,22 +3,31 @@ import sys
 
 n, k = map(int, input().split())
 
-home = [tuple(map(int, input().split())) for _ in range(n)]
-
-# home에서 k개를 combinations로 선택
 def dist(x1, y1, x2, y2):
     return abs(x1-x2)+abs(y1-y2)
 
-ans = sys.maxsize
-for lst in itertools.combinations(home, k):
-    res = dict()
-    for x, y in home:
-        if (x, y) not in lst:
-            for lx, ly in lst:
-                if (x, y) not in res:
-                    res[(x, y)] = dist(x, y, lx, ly)
-                else:
-                    res[(x, y)] = min(res[(x, y)], dist(x, y, lx, ly))
-    ans = min(ans, max(res.values()))
+home = []
+nums = dict()
+for i in range(n):
+    x, y = map(int, input().split())
+    home.append((x, y))
+    nums[(x, y)] = i
 
+INF = sys.maxsize
+ans = INF
+for selected in itertools.combinations(home, k):
+    sets = set(nums[(x, y)] for x, y in selected)
+    arr = [INF] * n
+    for i in range(n):
+        if i in sets:
+            continue
+        for sx, sy in selected:
+            arr[i] = min(arr[i], dist(sx, sy, home[i][0], home[i][1]))
+    res = 0
+    for i in range(n):
+        if arr[i] != INF:
+            res = max(arr[i], res)
+    ans = min(ans, res)
 print(ans)
+
+
